d5c31289ca65434262289273e24a47705f6efc7d
ranielcsar <ranielcsardev@gmail.com>
2024-08-02 15:36:01 -0300

    feat: repo do curso

--- src/infra/repository/course-repository.ts
+++ src/infra/repository/course-repository.ts
@@ -3,22 +3,25 @@ import DatabaseConnection from "@/infra/db/db-connection"
 import crypto from "crypto"
 
 export interface CourseRepository {
-    create(input: any): Promise<any>
+    create(input: any): Promise<boolean>
     list(): Promise<any>
     get(id: string): Promise<any>
+    update(id: string, input: any): Promise<any>
 }
 
 export class CourseRepositoryDb implements CourseRepository {
     constructor(readonly connection: DatabaseConnection) {}
 
-    async create(input: any): Promise<any> {
-        const course = Course.create(input.name, input.category, input.status)
-        await this.connection.query(
-            "INSERT INTO courses.course (course_id, name, category, status) values ($1, $2, $3, $4)",
-            [course.getId(), course.getName(), course.getCategory(), course.getStatus()]
-        )
-        return {
-            courseId: course.getId(),
+    async create(input: Course): Promise<boolean> {
+        try {
+            await this.connection.query(
+                "INSERT INTO courses.course (course_id, name, category, status) values ($1, $2, $3, $4)",
+                [input.getId(), input.getName(), input.getCategory(), input.getStatus()]
+            )
+            // this.connection.close()
+            return true
+        } catch {
+            return false
         }
     }
 
@@ -27,6 +30,7 @@ export class CourseRepositoryDb implements CourseRepository {
             "SELECT * FROM courses.course LIMIT 10",
             []
         )
+        this.connection.close()
         return courses.map((c: any) =>
             Course.restore(c.course_id, c.name, c.category, c.status, c.lessons)
         )
@@ -37,6 +41,7 @@ export class CourseRepositoryDb implements CourseRepository {
             "SELECT * FROM courses.course WHERE course_id = $1",
             [id]
         )
+        // this.connection.close()
         if (!course) return
         return Course.restore(
             course.course_id,
@@ -46,10 +51,18 @@ export class CourseRepositoryDb implements CourseRepository {
             course.lessons
         )
     }
+
+    async update(id: string, input: any): Promise<any> {
+        await this.connection.query(
+            "UPDATE courses.course SET name = $1, category = $2, status = $3 WHERE course_id = $4",
+            [input.name, input.category, input.status, id]
+        )
+        // this.connection.close()
+    }
 }
 
 export class CourseRepositoryMemory implements CourseRepository {
-    courses: any[] = []
+    courses: Course[] = []
 
     async create(input: any): Promise<any> {
         const courseId = crypto.randomUUID()
@@ -62,7 +75,17 @@ export class CourseRepositoryMemory implements CourseRepository {
     }
 
     async get(id: string): Promise<any> {
-        const course = this.courses.find((c) => c.courseId === id)
+        const course = this.courses.find((c) => c.getId() === id)
         return course
+    }
+
+    async update(id: string, input: any): Promise<any> {
+        this.courses = this.courses.map((c) => {
+            if (c.getId() === id) {
+                return { courseId: id, ...input }
+            }
+
+            return c
+        })
     }
 }
