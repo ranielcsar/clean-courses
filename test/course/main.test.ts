defabcc379e30f64985790a80ec0eeaaf954aad9
ranielcsar <ranielcsardev@gmail.com>
2024-08-15 06:57:52 -0300

    test: mudando pra "em memória" usando fake

--- test/course/main.test.ts
+++ test/course/main.test.ts
@@ -2,9 +2,9 @@ import { CategoryEnum, StatusEnum } from "@/domain/enum"
 import { CreateNewCourse, GetCourse, UpdateCourse } from "@/app/usecases/course"
 import {
     CourseRepositoryMemory,
-    CourseRepositoryDb,
+    // CourseRepositoryDb,
 } from "@/infra/repository/course-repository"
-import { PgPromiseAdapter } from "@/infra/db/db-connection"
+// import { PgPromiseAdapter } from "@/infra/db/db-connection"
 
 function randomValueFromEnum(enumarator: Record<string, string>) {
     let key: string | string[] = Object.keys(enumarator)
@@ -15,10 +15,11 @@ function randomValueFromEnum(enumarator: Record<string, string>) {
 let createCourse: CreateNewCourse
 let getCourse: GetCourse
 let updateCourse: UpdateCourse
+let courseRepo: CourseRepositoryMemory
 
 beforeEach(async () => {
-    const conn = new PgPromiseAdapter()
-    const courseRepo = new CourseRepositoryDb(conn)
+    // const conn = new PgPromiseAdapter()
+    courseRepo = new CourseRepositoryMemory()
     createCourse = new CreateNewCourse(courseRepo)
     getCourse = new GetCourse(courseRepo)
     updateCourse = new UpdateCourse(courseRepo)
@@ -33,11 +34,13 @@ test("Deve criar um novo curso", async () => {
     }
     const outputCreate = await createCourse.execute(input)
     expect(outputCreate.courseId).toBeDefined()
+    const createdCourse = await getCourse.execute(outputCreate.courseId)
+    expect(createdCourse?.getName()).toBe(input.name)
+    expect(createdCourse?.getCategory()).toBe(input.category)
+    expect(createdCourse?.getStatus()).toBe(input.status)
 })
 
 test("Deve listar cursos", async () => {
-    const conn = new PgPromiseAdapter()
-    const courseRepo = new CourseRepositoryDb(conn)
     const outputList = await courseRepo.list()
     expect(Array.isArray(outputList)).toBe(true)
 })
@@ -55,7 +58,7 @@ test("Deve atualizar um curso existente", async () => {
     input.name = `Curso ${Math.floor(Math.random() * 1200)}`
     await updateCourse.execute(outputCreate.courseId, input)
     const updatedCourse = await getCourse.execute(outputCreate.courseId)
-    expect(updatedCourse.name).toBe(input.name)
-    expect(updatedCourse.category).toBe(input.category)
-    expect(updatedCourse.status).toBe(input.status)
+    expect(updatedCourse?.getName()).toBe(input.name)
+    expect(updatedCourse?.getCategory()).toBe(input.category)
+    expect(updatedCourse?.getStatus()).toBe(input.status)
 })
